f1757e73e428c6273562b7917e9f4e9eb2775912
Arjun Jakkipally <68869672+arjunj132@users.noreply.github.com>
2022-08-04 19:06:22 -0400

    Update nitrogen.py

--- nitrogen.py
+++ nitrogen.py
@@ -56,8 +56,8 @@ def texteditor():
     
     def run_file():
         input = txt_edit.get("1.0",END)
-        #old_stdout = sys.stdout
-        #sys.stdout = buffer = io.StringIO()
+        old_stdout = sys.stdout
+        sys.stdout = buffer = io.StringIO()
         with add_path(sys.path[0] + "/oxygen"):
             from oxygen import Oxygen
             oxygen = Oxygen("\n" + input + "\n")
@@ -66,11 +66,11 @@ def texteditor():
                 print("Lexer: " + result["error"])
                 sys.exit()
             oxygen.parse(result["tokens"])
-        #sys.stdout = old_stdout
-        #whatWasPrinted = buffer.getvalue()
-        #tkinter.messagebox.showinfo("Nitrogen - Result",  whatWasPrinted)
+        sys.stdout = old_stdout
+        whatWasPrinted = buffer.getvalue()
+        tkinter.messagebox.showinfo("Nitrogen - Result",  whatWasPrinted)
         
-        #buffer.close()
+        buffer.close()
     
     window = Tk()
     window.title("Nitrogen")
@@ -159,8 +159,7 @@ Hello! You are installing a fresh new copy of the Oxygen Programming Language. P
 
 
 What's new:
-The OPL and the first version of Oxygen is released.
-
+https://github.com/arjunj132/oxygen/releases/
 
 
 How to use:
